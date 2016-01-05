7c221834ce13c760acb0bdfe5487104544b3cc3a
we20 <wez20@mihanmail.ir>
2016-01-05 18:34:37 +0330

    Update Version.lua

--- plugins/Version.lua
+++ plugins/Version.lua
@@ -4,7 +4,7 @@ function run(msg, matches)
   return 'SilVeR robot v.1'.. [[ 
 This is Old Bot OF Telegram.
   This bot CreaTed By:
-  @MR_SILVER1[RePoRt]
+  @omidtarh[RePoRt]
   This BoT Is A BesT BoT
 end
 
